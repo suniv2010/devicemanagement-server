46b3b386c68300e6f24a9d750a34092efec60639
Sunitha Vibaker <sunitha.vibaker@provise.com>
2021-02-12 02:08:41 +0530

    service

--- src/routes/device.routes.js
+++ src/routes/device.routes.js
@@ -2,12 +2,23 @@ const express = require('express')
 const router = express.Router()
 const deviceController = require('../controllers/device.controllers');
 
-// Retrieve all users
+// Retrieve all device
 router.get('/', deviceController.findAll);
 
 
-// Create a new user
+// Create a new device
 router.post('/', deviceController.create);
+
+// Retrieve a single user with id
+router.get('/:id', deviceController.findOne);
+
+// Update a device with id
+router.put('/:id', deviceController.update);
+
+// Delete a device with id
+router.delete('/:id', deviceController.delete);
+
+module.exports = router
 
 
 
